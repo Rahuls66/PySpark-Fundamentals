487f654a05866712ec987b4e31ef8bc88f02521a
Rahul Shah <rahul.shah4513@gmail.com>
2021-09-08 04:08:46 +0530

    Update ImportCSV.py

--- ImportCSV.py
+++ ImportCSV.py
@@ -2,4 +2,4 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName('Import CSV Exercise').getOrCreate()
 
-authors = spark.read.csv('authors.csv', sep = ',', inferSchema = True, header = True)
+authors = spark.read.csv(<fileName.csv>, sep = ',', inferSchema = True, header = True)
